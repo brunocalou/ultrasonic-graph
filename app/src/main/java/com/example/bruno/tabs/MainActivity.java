0cab3cf9d2c6540f32d5b5c2d2af7a3081dfa3df
brunocalou <brunocaloualves@gmail.com>
2016-08-24 14:18:13 -0300

    Fixed navigation drawer item colors by separating the toolbar theme in a different style tag

--- app/src/main/java/com/example/bruno/tabs/MainActivity.java
+++ app/src/main/java/com/example/bruno/tabs/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bruno.tabs;
 
+import android.content.res.ColorStateList;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
